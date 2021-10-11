ff9d8f3c8375646f91d0a18f7cdcbe9e8e32dd9e
Rudra Dave <dave.rudra-kashyapbhai@capgemini.com>
2021-10-11 15:48:56 +0530

    Task-1 Complete

--- Point/src/com/point/Point2D.java
+++ Point/src/com/point/Point2D.java
@@ -3,33 +3,40 @@ package com.point;
 
 public class Point2D {
 	
+	// step-1
 	private int x, y;
 	
 	public Point2D(int x, int y) {
 		this.x = x;
         this.y = y;
 	}
-	
-	public Point2D createPoint(int x, int y) {
-		Point2D p = new Point2D(x, y);
-		return p;
+
+	//step-2
+	public String getDetails() {
+		return "Point ("+x+","+y+")";
 	}
 	
-    public double calculateDistance (Point2D that) {
-        double dx = this.x - that.x;
-        double dy = this.y - that.y;
-        return Math.sqrt(dx*dx + dy*dy);
-    }
-
-    public boolean isEqual(Point2D that) {
+	//step-3
+	public boolean isEqual(Point2D that) {
     	if (this.getDetails().equals(that.getDetails())) {
 			return true;
 		}
 		return false;
     }
 
-	public String getDetails() {
-		return "Point ("+x+","+y+")";
+	//step-4
+	public Point2D createPoint(int x, int y) {
+		Point2D p = new Point2D(x, y);
+		return p;
 	}
+	
+	//step-5
+    public double calculateDistance (Point2D that) {
+        double dx = this.x - that.x;
+        double dy = this.y - that.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+
+    
 
 }
--- Point/src/com/point/TestPoint.java
+++ Point/src/com/point/TestPoint.java
@@ -5,30 +5,32 @@ import java.util.Scanner;
 public class TestPoint {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
+		//step-6
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter x and y for point 1: ");
 		int p1x = sc.nextInt();
 		int p1y = sc.nextInt();
 		Point2D p1 = new Point2D(p1x,p1y);
+		//step-7
 		System.out.println("P1 " + p1.getDetails());
 
 		System.out.println("Enter x and y for point 2: ");
 		int p2x = sc.nextInt();
 		int p2y = sc.nextInt();
 		Point2D p2 = new Point2D(p2x, p2y);
+		//step-7
 		System.out.println("P2 " + p2.getDetails());
 		
+		//step-8
 		System.out.println("Are both points ? -> " + p1.isEqual(p2));
 		
+		//step-9
 		Point2D p3 = p1.createPoint(p1x, p1y);
 		
-		
+		//step-10
 		System.out.println("Distance between p1 and p2 is: " + p1.calculateDistance(p2));
 		System.out.println("Distance between p2 and p3 is: " + p2.calculateDistance(p3));
 		System.out.println("Distance between p1 and p3 is: " + p1.calculateDistance(p3));
-
 	}
-
 }
