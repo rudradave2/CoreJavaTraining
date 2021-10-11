db46455411dd2775e5ef421867e5177d60d80078
Rudra Dave <dave.rudra-kashyapbhai@capgemini.com>
2021-10-11 14:39:27 +0530

    Task-1 complete

--- Point/src/com/point/Point2D.java
+++ Point/src/com/point/Point2D.java
@@ -5,24 +5,31 @@ public class Point2D {
 	
 	private int x, y;
 	
-	public Point2D() {
-		
+	public Point2D(int x, int y) {
+		this.x = x;
+        this.y = y;
 	}
 	
-	public Point2D(int i, int j) {
-		x = i;
-		y = j;
+	public Point2D createPoint(int x, int y) {
+		Point2D p = new Point2D(x, y);
+		return p;
 	}
 	
-	public String getDetails() {
-		return "Point ("+x+","+y+")";
-	}
-	
-	public boolean isEqual(String p) {
-		if (p.equals(getDetails())) {
+    public double calculateDistance (Point2D that) {
+        double dx = this.x - that.x;
+        double dy = this.y - that.y;
+        return Math.sqrt(dx*dx + dy*dy);
+    }
+
+    public boolean isEqual(Point2D that) {
+    	if (this.getDetails().equals(that.getDetails())) {
 			return true;
 		}
 		return false;
+    }
+
+	public String getDetails() {
+		return "Point ("+x+","+y+")";
 	}
 
 }
---
+++ Point/src/com/point/TestPoint.java
@@ -0,0 +1,34 @@
+package com.point;
+
+import java.util.Scanner;
+
+public class TestPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter x and y for point 1: ");
+		int p1x = sc.nextInt();
+		int p1y = sc.nextInt();
+		Point2D p1 = new Point2D(p1x,p1y);
+		System.out.println("P1 " + p1.getDetails());
+
+		System.out.println("Enter x and y for point 2: ");
+		int p2x = sc.nextInt();
+		int p2y = sc.nextInt();
+		Point2D p2 = new Point2D(p2x, p2y);
+		System.out.println("P2 " + p2.getDetails());
+		
+		System.out.println("Are both points ? -> " + p1.isEqual(p2));
+		
+		Point2D p3 = p1.createPoint(p1x, p1y);
+		
+		
+		System.out.println("Distance between p1 and p2 is: " + p1.calculateDistance(p2));
+		System.out.println("Distance between p2 and p3 is: " + p2.calculateDistance(p3));
+		System.out.println("Distance between p1 and p3 is: " + p1.calculateDistance(p3));
+
+	}
+
+}
